9c2caa187867179554995f6a1e5fbc48032b40c0
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-19 16:10:04 +0100

    Added possibility to specify which node version to install

--- node-install.sh
+++ node-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Taken from https://www.w3schools.com/nodejs/nodejs_raspberrypi.asp
-# Version presently set manually
-udo apt-get update
+version=$1
+
+if [ -z "$version" ]; then
+  version=14
+fi
+
+echo "Installing Nod.js version $version"
+
+# Below is taken from https://www.w3schools.com/nodejs/nodejs_raspberrypi.asp
+sudo apt-get update
 sudo apt-get dist-upgrade
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_$version.x | sudo -E bash -
 sudo apt-get install -y nodejs
